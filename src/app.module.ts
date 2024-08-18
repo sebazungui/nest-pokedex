8c113b8d0e30439a0bb27037cbe41776808c77f3
Sebastian Robles <sebaroblesca@gmail.com>
2024-08-18 17:27:05 -0400

    Antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 @Module({
@@ -15,7 +16,9 @@ import { CommonModule } from './common/common.module';
   
   PokemonModule,
   
-  CommonModule]
+  CommonModule,
+  
+  SeedModule]
 
 })
 export class AppModule { }
